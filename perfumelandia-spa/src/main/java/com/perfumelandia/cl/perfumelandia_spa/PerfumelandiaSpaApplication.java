c369a25e14bb7e8c3aa6fc5b5e6112da2c0367b4
diego mancilla <die.mancilla@duocuc.cl>
2025-05-12 14:07:44 -0400

    hola diego 2

--- perfumelandia-spa/src/main/java/com/perfumelandia/cl/perfumelandia_spa/PerfumelandiaSpaApplication.java
+++ perfumelandia-spa/src/main/java/com/perfumelandia/cl/perfumelandia_spa/PerfumelandiaSpaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PerfumelandiaSpaApplication {
-//Hola soy diego Mancilla.
+//Hola soy diego Mancilla....
 	public static void main(String[] args) {
 		SpringApplication.run(PerfumelandiaSpaApplication.class, args);
 	}
