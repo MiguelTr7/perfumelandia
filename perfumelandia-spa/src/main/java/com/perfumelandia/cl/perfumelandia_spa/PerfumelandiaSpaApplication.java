16219441f9a149b963da403a0a1eceb4830dce20
perfumelandia <mi.tropa@duocuc.cl>
2025-05-12 13:32:33 -0400

    PRIMER COMMIT

---
+++ perfumelandia-spa/src/main/java/com/perfumelandia/cl/perfumelandia_spa/PerfumelandiaSpaApplication.java
@@ -0,0 +1,13 @@
+package com.perfumelandia.cl.perfumelandia_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumelandiaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumelandiaSpaApplication.class, args);
+	}
+
+}
