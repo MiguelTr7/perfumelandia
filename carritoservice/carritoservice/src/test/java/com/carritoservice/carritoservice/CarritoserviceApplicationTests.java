2f23ff7930efc8a267e990a393ebd1632548188d
DiegoTechSolutions <die.mancilla@duocuc.cl>
2025-05-22 12:48:34 -0400

    Agrega clase modelo Carrito y corrige importaciones en CarritoRepository y agrege todas las carpetas necesarias para las importaciones de libreria, ademas de todas las clases java y utilizando los metodos de SQL y postman de carrito

---
+++ carritoservice/carritoservice/src/test/java/com/carritoservice/carritoservice/CarritoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carritoservice.carritoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
