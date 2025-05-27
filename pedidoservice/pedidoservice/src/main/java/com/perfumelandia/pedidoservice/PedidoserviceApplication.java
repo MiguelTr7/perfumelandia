2f7c85f8e148062d15c468110ec6933b848e554c
MaicolHernandez33 <mai.hernandezz@duocuc.cl>
2025-05-27 17:25:24 -0400

    se agrego la conexión entre microservico de usuario y pedido

--- pedidoservice/pedidoservice/src/main/java/com/perfumelandia/pedidoservice/PedidoserviceApplication.java
+++ pedidoservice/pedidoservice/src/main/java/com/perfumelandia/pedidoservice/PedidoserviceApplication.java
@@ -2,6 +2,8 @@ package com.perfumelandia.pedidoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PedidoserviceApplication {
@@ -10,7 +12,11 @@ public class PedidoserviceApplication {
 		SpringApplication.run(PedidoserviceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
 }
 
 
-//Hola...
