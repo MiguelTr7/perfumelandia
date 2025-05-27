9d13f6ed4a2d50998abf4148a08c7b672978b69c
MaicolHernandez33 <mai.hernandezz@duocuc.cl>
2025-05-27 12:38:51 -0400

    Tercer intenti de metodo id

--- pedidoservice/pedidoservice/src/main/java/com/perfumelandia/pedidoservice/controller/PedidoController.java
+++ pedidoservice/pedidoservice/src/main/java/com/perfumelandia/pedidoservice/controller/PedidoController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.List;
+        import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -57,3 +57,4 @@ public class PedidoController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
+//3 INTENTO
